7ba23cc55ce7ce7f6e41976c173dda2addd54a73
jcarzua <jcarzua@timesolution.com.ar>
2021-07-01 19:40:12 -0300

    Se modifica gitignore, estaba ignorando Services/Log
    (cherry picked from commit 4d8e6a009b766506badc6f000110bda17c64cd71)

---
+++ EvaluacionTecnica.Services/Services/Log/LoggerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EvaluacionTecnica.Services.Services.Log
+{
+    public class LoggerService : ILoggerService
+    {
+        public void Registrar(string detalle)
+        {
+            Console.Write(detalle);
+        }
+    }
+}
